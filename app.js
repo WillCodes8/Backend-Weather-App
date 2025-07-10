50dd4f8f7d8a261fb4b2f9e341a4242901501a33
William <wwashing@terpmail.umd.edu>
2025-07-10 18:43:00 -0400

    Public Demo Page & Landing Page Added

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Weather Tracker API is running');
+  res.render('index', { title: 'Backend Weather Tracker App' });
+});
+
+app.get('/public', (req, res) => {
+  res.render('public', { title: 'Public Weather Page' });
 });
 
 app.listen(port, () => {
